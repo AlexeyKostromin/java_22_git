75e35218662a468920892229755a26d6f902cc6e
Alexey Kostromin <kostromin.aleksei@gmail.com>
2023-10-18 21:47:41 +0200

    change local2

--- src/main/java/com/alexey_k/Main.java
+++ src/main/java/com/alexey_k/Main.java
@@ -2,6 +2,6 @@ package com.alexey_k;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! - updated master on local");
+        System.out.println("Hello world! - updated master on local2");
     }
 }
